6ae281c963794016adb5a0356b8c1d1f7c72e03e
Yurev-Timofey <yurev.timofey@gmail.com>
2020-12-14 01:49:29 +0300

    Добавляю основную функциональность

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class Moveable {
     constructor(object_id) {
-        this.object_id = object_id;
-        this.element = document.getElementById(object_id);
+        this.object_id = object_id
+        this.element = document.getElementById(object_id)
     }
 
     getX() {
@@ -13,14 +13,14 @@ class Moveable {
     }
 
     moveAt(x, y) {
-        this.element.style.left = x + 'px';
-        this.element.style.top = y + 'px';
+        this.element.style.left = x + 'px'
+        this.element.style.top = y + 'px'
     }
 
     moveWith(offsetX, offsetY) {
-        let x = this.element.getBoundingClientRect().left - offsetX;
-        let y = this.element.getBoundingClientRect().top - offsetY;
-        this.moveAt(x, y);
+        let x = this.element.getBoundingClientRect().left - offsetX
+        let y = this.element.getBoundingClientRect().top - offsetY
+        this.moveAt(x, y)
     }
 
     moveToCenter(pageX, pageY) {
@@ -34,115 +34,172 @@ class Draggable extends Moveable {
     constructor(object_id) {
         super(object_id)
 
-        this.currentDroppable = null;
-        this.inDroppable = false;
+        this.currentDroppable = null
+        this.inDroppable = false
 
-        this.onMouseMoveWithBind = this.onMouseMove.bind(this); //Мини костыль
+        this.onMouseMoveWithBind = this.onMouseMove.bind(this) //Мини костыль
 
-        this.element.addEventListener("mousedown", this.mousedown.bind(this));
-        this.element.addEventListener("mouseup", this.mouseup.bind(this));
-        this.element.addEventListener("dragstart", this.dragstart);
-    }
-
-    moveAt(x, y) {
-        let oldX = this.getX();
-        let oldY = this.getY();
-
-        super.moveAt(x, y);
-
-        this.element.dispatchEvent(new CustomEvent("moved", {
-            detail: { shiftX: oldX - this.getX(), shiftY: oldY - this.getY() }
-        }))
+        this.element.addEventListener("mousedown", this.mousedown.bind(this))
+        this.element.addEventListener("mouseup", this.mouseup.bind(this))
+        this.element.addEventListener("dragstart", this.dragstart)
     }
 
     onMouseMove(event) {
-        this.moveWithCursorShift(event);
+        this.moveWithCursorShift(event)
 
-        this.element.hidden = true;
-        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-        this.element.hidden = false;
+        this.element.hidden = true
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY)
+        this.element.hidden = false
 
-        if (!elemBelow) return;
+        if (!elemBelow) return
 
-        let droppableBelow = elemBelow.closest('.dot');
+        let droppableBelow = elemBelow.closest('.dot')
         if (this.currentDroppable != droppableBelow) {
             if (this.currentDroppable) { // null если мы были не над droppable до этого события
                 // (например, над пустым пространством)
-                this.leaveDroppable(this.currentDroppable);
+                this.leaveDroppable(this.currentDroppable)
             }
 
-            this.currentDroppable = droppableBelow;
+            this.currentDroppable = droppableBelow
+
+
             if (this.currentDroppable) { // null если мы не над droppable сейчас, во время этого события
                 // (например, только что покинули droppable)
-                this.enterDroppable(this.currentDroppable);
+                this.enterDroppable(this.currentDroppable)
             }
         }
     }
 
     mousedown(event) {
-        this.shiftX = event.clientX - this.element.getBoundingClientRect().left;
-        this.shiftY = event.clientY - this.element.getBoundingClientRect().top;
+        this.shiftX = event.clientX - this.element.getBoundingClientRect().left
+        this.shiftY = event.clientY - this.element.getBoundingClientRect().top
 
-        this.element.style.position = 'absolute';
-        this.element.style.zIndex = 1000;
-        document.body.append(this.element);
+        this.element.style.position = 'absolute'
+        this.element.style.zIndex = 1000
+        document.body.append(this.element)
 
-        this.moveWithCursorShift(event);
+        this.moveWithCursorShift(event)
 
-        document.addEventListener('mousemove', this.onMouseMoveWithBind, true);
+        document.addEventListener('mousemove', this.onMouseMoveWithBind, true)
     }
 
     dragstart(event) {
-        event.preventDefault();
+        event.preventDefault()
     }
 
-    mouseup(_event) {
-        document.removeEventListener('mousemove', this.onMouseMoveWithBind, true);
+    mouseup() {
+        document.removeEventListener('mousemove', this.onMouseMoveWithBind, true)
 
         if (this.inDroppable) {
-            let boundingRect = this.currentDroppable.getBoundingClientRect();
-            this.moveToCenter(boundingRect.left + boundingRect.width / 2, boundingRect.top + boundingRect.height / 2);
+            this.attachedToDroppable()
         }
     }
 
     moveWithCursorShift(event) {
-        this.moveAt(event.pageX - this.shiftX, event.pageY - this.shiftY);
+        this.moveAt(event.pageX - this.shiftX, event.pageY - this.shiftY)
     }
 
     enterDroppable(elem) {
-        elem.style.background = 'pink';
-        this.inDroppable = true;
+        elem.style.background = 'pink'
+        this.inDroppable = true
     }
 
     leaveDroppable(elem) {
-        elem.style.background = '';
-        this.inDroppable = false;
+        elem.style.background = ''
+        this.inDroppable = false
+
+        if (this.attachedToDroppable)
+            this.detachedFromDroppable()
+    }
+
+    attachedToDroppable() {
+        let boundingRect = this.currentDroppable.getBoundingClientRect()
+        this.moveToCenter(boundingRect.left + boundingRect.width / 2, boundingRect.top + boundingRect.height / 2)
+
+        this.attachedToDroppable = true
+    }
+
+    detachedFromDroppable() {
+        this.attachedToDroppable = false
     }
 
 }
 
-class DragPoint extends Draggable {}
+class DragPoint extends Draggable {
+    constructor(object_id, part) {
+        super(object_id)
+
+        this.attachedPart = part
+    }
+
+    moveAt(x, y) {
+        let oldX = this.getX()
+        let oldY = this.getY()
+
+        super.moveAt(x, y)
+
+        this.element.dispatchEvent(new CustomEvent("circleMoved", {
+            detail: { shiftX: oldX - this.getX(), shiftY: oldY - this.getY() }
+        }))
+    }
+
+    moveWith(offsetX, offsetY) { //moveWith переопределён для того, чтобы не вызывать event
+        let x = this.element.getBoundingClientRect().left - offsetX
+        let y = this.element.getBoundingClientRect().top - offsetY
+
+        this.element.style.left = x + 'px'
+        this.element.style.top = y + 'px'
+    }
+
+    // moveWithCursorShift(event) {
+    //     let oldX = this.getX()
+    //     let oldY = this.getY()
+
+    //     super.moveWithCursorShift(event)
+
+    //     this.element.dispatchEvent(new CustomEvent("circleMoved", {
+    //         detail: { shiftX: oldX - this.getX(), shiftY: oldY - this.getY() }
+    //     }))
+    // }
+
+    attachedToDroppable() {
+        super.attachedToDroppable()
+
+        this.currentDroppable.dispatchEvent(new CustomEvent("dotConnected", {
+            detail: { part: this.attachedPart }
+        }))
+    }
+
+    detachedFromDroppable() {
+        super.detachedFromDroppable()
+
+        this.currentDroppable.dispatchEvent(new CustomEvent("dotDisconnected", {
+            detail: { part: this.attachedPart }
+        }))
+    }
+}
 
 class DropPoint extends Moveable {}
 
 class PartPicture extends Moveable {
+
     getLeftPointX() {
-        return this.element.getBoundingClientRect().left;
+        return this.element.getBoundingClientRect().left
     }
 
     getRightPointX() {
-        return this.element.getBoundingClientRect().right;
+        return this.element.getBoundingClientRect().right
     }
 
     getPointY() {
-        return this.element.getBoundingClientRect().top + this.element.getBoundingClientRect().height / 2;
+        return this.element.getBoundingClientRect().top + this.element.getBoundingClientRect().height / 2
     }
 }
 
 class DragPointWithFilter extends DragPoint {
-    constructor(object_id, color) {
-        super(object_id);
-        this.color = color;
+    constructor(object_id, part, color) {
+        super(object_id, part)
+        this.color = color
     }
 
     enterDroppable(elem) {
@@ -151,47 +208,206 @@ class DragPointWithFilter extends DragPoint {
     }
 }
 
-
 class Part {
-    constructor(partName) {
-        this.partName = partName;
+    constructor(partName, assembly) {
+        this.partName = partName
+        this.assembly = null //Ссылка на двусвязный список, в котором находится эта деталь
 
-        this.dotId = "dot_" + partName;
-        this.circleId = "circle_" + partName;
-        this.partPictureId = "partPicture_" + partName;
+        this.next = null //Part является нодой двусвязного списка
+        this.previous = null
 
-        let dotHtml = '<img src="graphics/dot_blue.png" id="' + this.dotId + '" class="dot blue">';
-        let circleHtml = '<img src="graphics/circle_blue.png" id="' + this.circleId + '" class="circle blue">';
-        let partPictureHtml = '<img src="graphics/' + partName + '.png" id="' + this.partPictureId + '" class="partPicture">';
-        this.createHtmlElement(dotHtml);
-        this.createHtmlElement(circleHtml);
-        this.createHtmlElement(partPictureHtml);
+        this.dotId = "dot_" + partName
+        this.circleId = "circle_" + partName
+        this.partPictureId = "partPicture_" + partName
+
+        let dotHtml = '<img src="graphics/dot_blue.png" id="' + this.dotId + '" class="dot blue">'
+        let circleHtml = '<img src="graphics/circle_blue.png" id="' + this.circleId + '" class="circle blue">'
+        let partPictureHtml = '<img src="graphics/' + partName + '.png" id="' + this.partPictureId + '" class="partPicture">'
+        this.createHtmlElement(dotHtml)
+        this.createHtmlElement(circleHtml)
+        this.createHtmlElement(partPictureHtml)
 
         this.dot = new DropPoint(this.dotId)
-        this.circle = new DragPoint(this.circleId);
-        this.part = new PartPicture(this.partPictureId);
+        this.circle = new DragPoint(this.circleId, this)
+        this.part = new PartPicture(this.partPictureId)
 
-        this.part.moveAt(200, 200);
+        this.part.moveAt(200, 200)
 
-        this.dot.moveToCenter(this.part.getLeftPointX(), this.part.getPointY());
-        this.circle.moveToCenter(this.part.getRightPointX(), this.part.getPointY());
+        this.dot.moveToCenter(this.part.getLeftPointX(), this.part.getPointY())
+        this.circle.moveToCenter(this.part.getRightPointX(), this.part.getPointY())
 
-        this.circle.element.addEventListener("moved", this.onCircleMove.bind(this))
+        this.circle.element.addEventListener("circleMoved", this.onCircleMove.bind(this))
+        this.dot.element.addEventListener("dotConnected", this.onDotConnencted.bind(this))
+        this.dot.element.addEventListener("dotDisconnected", this.onDotDisconnected.bind(this))
     }
 
     onCircleMove(event) {
-        this.dot.moveWith(event.detail.shiftX, event.detail.shiftY);
-        this.part.moveWith(event.detail.shiftX, event.detail.shiftY);
+        let shiftX = event.detail.shiftX
+        let shiftY = event.detail.shiftY
+
+        this.dot.moveWith(shiftX, shiftY)
+        this.part.moveWith(shiftX, shiftY)
+        if (this.next)
+            this.assembly.onPartMove(shiftX, shiftY, this) //Можно было бы реализовать через ивенты, но не вижу в этом смысла
     }
 
+    onAssemblyMove(shiftX, shiftY) {
+        this.circle.moveWith(shiftX, shiftY)
+        this.dot.moveWith(shiftX, shiftY)
+        this.part.moveWith(shiftX, shiftY)
+    }
 
+    onDotConnencted(event) {
+        // this.previous = event.detail.part
+        // event.detail.part.next = this
+
+        this.assembly.merge(event.detail.part.assembly)
+
+        event.detail.part.assembly = this.assembly
+    }
+
+    onDotDisconnected(event) {
+        let eventPart = event.detail.part //TODO нормально тут всё назвать
+
+        let newAssembly = new Assembly()
+        newAssembly.copyAfter(eventPart.assembly, eventPart)
+        eventPart.assembly = newAssembly
+
+        this.assembly.removeAfter(eventPart)
+    }
 
     createHtmlElement(innerHTML) {
-        let element = document.createElement('div');
-        element.innerHTML = innerHTML;
-        document.body.append(element);
+        let element = document.createElement('div')
+        element.innerHTML = innerHTML
+        document.body.append(element)
     }
 }
 
-new Part("sidenie");
-new Part("podlokotnik_big_left")
+// class Node {
+//     constructor(value) {
+//         this.value = value
+//         this.next = null
+//     }
+// }
+
+class Assembly { //Doubly linked list
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+
+    static createOnePartAssembly(partName) { //Я абсолютно не уверен, можно ли использовать так статические методы, но это мой код!!! Что хочу то и делаю!!!    
+        let assembly = new Assembly()
+
+        let part = new Part(partName)
+
+        assembly.addToTheEnd(part)
+        return assembly
+    }
+
+    onPartMove(shiftX, shiftY, movedPart) {
+        let current = this.head
+        while (current) {
+            if (current != movedPart)
+                current.onAssemblyMove(shiftX, shiftY)
+            current = current.next
+        }
+    }
+
+    addToTheEnd(part) {
+        part.assembly = this
+
+        if (this.length === 0) {
+            this.head = part
+        } else {
+            let current = this.head
+
+            while (current.next) {
+                current = current.next
+            }
+
+            current.next = part
+        }
+
+        this.length++
+    }
+
+    // contains(part) {
+    //     if (this.length === 0)
+    //         return false
+
+
+    //     let current = this.head
+
+    //     if (current === part)
+    //         return true
+
+    //     while (current.next) {
+    //         if (current === part)
+    //             return true
+
+    //         current = current.next
+    //     }
+
+    //     return false
+    // }
+
+    merge(assembly) {
+        let current = assembly.head
+        while (current) {
+            this.addToTheEnd(current)
+            current = current.next
+            this.length++
+        }
+    }
+
+    copyAfter(assembly, part) {
+        let current = assembly.head
+
+        while (current != part && current)
+            current = current.next
+
+        while (current) {
+            this.addToTheEnd(current)
+            current = current.next
+            this.length++
+        }
+    }
+
+    removeAfter(part) {
+        console.log(this.getLength())
+        let current = this.head
+        let previous = null
+
+        while (current != part && current) {
+            previous = current
+            current = current.next
+        }
+        previous.next = null
+        console.log(this.getLength())
+    }
+
+    getLength() {
+        let current = this.head
+        let length = 0;
+
+        while (current) {
+            current = current.next
+            length++;
+        }
+
+        return length
+    }
+}
+
+// let sidenie = new Part("sidenie")
+// let podlokotnik = new Part("podlokotnik_big_left")
+
+// var assembly = new Assembly()
+// assembly.addToTheEnd(sidenie)
+
+// var assembly2 = new Assembly()
+// assembly2.addToTheEnd(podlokotnik)
+Assembly.createOnePartAssembly("sidenie")
+Assembly.createOnePartAssembly("podlokotnik_big_left")
+Assembly.createOnePartAssembly("podlokotnik_thin_right")
